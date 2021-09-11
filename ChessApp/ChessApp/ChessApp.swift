9da3f8a27f2fb07f5afc7f8a9ac718c69043962f
Nikolai Puchko <sapientisat@yandex-team.ru>
2021-09-11 16:17:43 +0300

    Hello, world

---
+++ ChessApp/ChessApp/ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessApp.swift
+//  ChessApp
+//
+//  Created by Nikolai Puchko on 11.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChessApp/ChessApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChessApp
+//
+//  Created by Nikolai Puchko on 11.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
